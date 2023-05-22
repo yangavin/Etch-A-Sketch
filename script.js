1dc64c1cf647c5e36391760851dbd3d0b4d430a9
Spycrab-db <gavinyan2004@gmail.com>
2023-05-22 12:52:52 -0400

    Implemented function for filling board and allowed for drawing by hovering.

--- script.js
+++ script.js
@@ -3,25 +3,36 @@ const dimensionLabel = document.querySelector('#dimension-label');
 const board = document.querySelector('#board');
 const clearButton = document.querySelector('#clear');
 
-dimensionSlider.addEventListener('input', ()=>{
+function fillBoard(dimension) {
+    board.innerHTML = '';
+    let pixelDimension = 100 / dimension;
+    for (let i = 1; i <= dimension ** 2; i++) {
+        let pixel = document.createElement('div');
+        pixel.classList.add('pixel');
+        pixel.addEventListener('mouseover', () => {
+            pixel.style.backgroundColor = 'black';
+        })
+        pixel.style.width = pixel.style.height = `${pixelDimension}%`;
+        board.append(pixel);
+    }
+}
+
+//Fill the board initially with 55x55
+fillBoard(55);
+
+
+dimensionSlider.addEventListener('input', () => {
     let dimension = dimensionSlider.value;
     dimensionLabel.textContent = `${dimension}x${dimension}`
 });
 
-dimensionSlider.addEventListener('mouseup', ()=>{
-    board.innerHTML = '';
-    let dimension = dimensionSlider.value;
-    let pixelDimension = 100/dimension;
-    for (let i = 1; i<=dimension**2; i++){
-        let pixel = document.createElement('div');
-        pixel.style.width = pixel.style.height = `${pixelDimension}%`;
-        board.append(pixel);
-    }
+dimensionSlider.addEventListener('mouseup', () => {
+    fillBoard(dimensionSlider.value);
 });
 
-clearButton.addEventListener('click', ()=>{
+clearButton.addEventListener('click', () => {
     const pixels = document.querySelectorAll('.pixel');
-    for (let pixel of pixels){
+    for (let pixel of pixels) {
         pixel.style.backgroundColor = 'white';
     }
 })
