5d8dbd0e65abd9467cc218b2071a697d7be32067
Spycrab-db <gavinyan2004@gmail.com>
2023-05-22 13:05:22 -0400

    Implemented script for changing paint color

--- script.js
+++ script.js
@@ -2,23 +2,30 @@ const dimensionSlider = document.querySelector('#dimension');
 const dimensionLabel = document.querySelector('#dimension-label');
 const board = document.querySelector('#board');
 const clearButton = document.querySelector('#clear');
+const colorInput = document.querySelector('#color');
 
-function fillBoard(dimension) {
+function resetBoard(dimension, color) {
     board.innerHTML = '';
     let pixelDimension = 100 / dimension;
     for (let i = 1; i <= dimension ** 2; i++) {
         let pixel = document.createElement('div');
         pixel.classList.add('pixel');
-        pixel.addEventListener('mouseover', () => {
-            pixel.style.backgroundColor = 'black';
-        })
         pixel.style.width = pixel.style.height = `${pixelDimension}%`;
         board.append(pixel);
     }
+    changeColor(color);
+}
+function changeColor(color){
+    const pixels = document.querySelectorAll('.pixel');
+    for (let pixel of pixels) {
+        pixel.addEventListener('mouseover', () => {
+            pixel.style.backgroundColor = color;
+        })
+    }
 }
 
-//Fill the board initially with 55x55
-fillBoard(55);
+//Fill the board initially with 55x55 and color black
+resetBoard(55, 'black');
 
 
 dimensionSlider.addEventListener('input', () => {
@@ -27,8 +34,12 @@ dimensionSlider.addEventListener('input', () => {
 });
 
 dimensionSlider.addEventListener('mouseup', () => {
-    fillBoard(dimensionSlider.value);
+    resetBoard(dimensionSlider.value, colorInput.value);
 });
+
+colorInput.addEventListener('change', ()=>{
+    changeColor(colorInput.value);
+})
 
 clearButton.addEventListener('click', () => {
     const pixels = document.querySelectorAll('.pixel');
